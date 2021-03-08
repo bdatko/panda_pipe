9e1c9bada3db742b9175248a8a02962ebbfead5c
datko <>
2021-03-08 16:27:17 -0500

    docs(setup.py): add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import pathlib
+
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
 
 setup(
     name="pandas_pipe",
     version="0.0.0",
     description="routine pandas method chain links wrapped with scikit-lego",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
     url="https://github.com/bdatko/panda_pipe",
     author="Benjamin Datko",
     author_email="ben.datko@gmail.com",
